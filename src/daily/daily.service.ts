1ba868ea58fb9df7b9b38fe125d63289669a7741
lirh42 <undercurre@163.com>
2024-10-08 15:47:14 +0800

    fix: userdata的创建逻辑

--- src/daily/daily.service.ts
+++ src/daily/daily.service.ts
@@ -55,8 +55,10 @@ export class UserDailyService {
         exercise_calories:
           createUserDailyDto.exercise_calories || recent.exercise_calories,
         music_time: createUserDailyDto.music_time || recent.music_time,
-        sleep_start_time: new Date(recent.sleep_start_time) || new Date(),
-        sleep_end_time: new Date(recent.sleep_end_time) || new Date(),
+        sleep_start_time:
+          new Date(recent.sleep_start_time) || recent.sleep_start_time,
+        sleep_end_time:
+          new Date(recent.sleep_end_time) || recent.sleep_end_time,
         id: shortid.generate(),
       });
     }
